8a09cde8488a3f3236a667f0c6aabea66ae3de59
AndrewZlobin <andrewzlobin1992@gmail.com>
2019-06-02 21:40:15 +0300

    Remade, using switch and build-in function mb_convert_case.

--- PHP/homework2/task2.php
+++ PHP/homework2/task2.php
@@ -5,21 +5,21 @@
 2) перевод всех символов в нижний регистр,
 3) перевод всех символов в нижний регистр и первых символов слов в верхний регистр.`*/
 
-$all_to_upper = function($string) {
-    return strtoupper($string);
-};
-$all_to_lower = function($string) {
-    return strtolower($string);
-};
-$only_first_upper = function($string) {
-    return ucfirst($string);
-};
-
 function string_converter ($result_string, $flag) {
-    return $flag($result_string);
+    switch ($flag) {
+        case "all_upper":
+            return mb_convert_case($result_string,MB_CASE_UPPER, "UTF-8");
+        case "all_lower":
+            return mb_convert_case($result_string,MB_CASE_LOWER, "UTF-8");
+        case "first_upper":
+            return mb_convert_case($result_string,MB_CASE_TITLE, "UTF-8");
+    }
 }
 
-var_dump(string_converter("bla-bla-bla", $all_to_upper));
-var_dump(string_converter("WHAT IS HAPPENING HERE?", $all_to_lower));
-var_dump(string_converter("A LIT BIT MORE CAPSLOCK!", $only_first_upper));
-var_dump(string_converter("just a text here", $only_first_upper));
+var_dump(string_converter("bla-bla-bla", "all_upper"));
+var_dump(string_converter("какой-то текст", "all_upper"));
+var_dump(string_converter("КТО ЗДЕСЬ?", "all_lower"));
+var_dump(string_converter("WHO IS HERE?", "all_lower"));
+var_dump(string_converter("A LIT BIT MORE CAPSLOCK!", "first_upper"));
+var_dump(string_converter("just a text here", "first_upper"));
+var_dump(string_converter("привет всем!", "first_upper"));
