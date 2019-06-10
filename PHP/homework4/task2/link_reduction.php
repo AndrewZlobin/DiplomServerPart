bcbce8aefd99e436f04df127a7547e37f9c12ea4
AndrewZlobin <andrewzlobin1992@gmail.com>
2019-06-10 22:44:21 +0300

    Task 2 is done (link reduction). Not perfect

--- PHP/homework4/task2/link_reduction.php
+++ PHP/homework4/task2/link_reduction.php
@@ -15,14 +15,16 @@ $file_treatment = function($filename){
     return $file_in_array;
 };
 
-function link_to_reduce($url, $filename, $func1, $func2){
+function link_to_reduce($url, $filename, $func1, $func2)
+{
 
-    //проверки на корректность введенных данных, которые я проверить не смог =(
-    if ($url == null){
+    $url_parse_for_check = parse_url($url);
+    //проверки на корректность введенных данных.
+    if (!$url_parse_for_check['path']) {
         print_r("Внимание!<br>Строка пуста<br>Повторите ввод!");
         return;
     }
-    if(!filter_var($url, FILTER_VALIDATE_URL)){
+    if (!filter_var($url, FILTER_VALIDATE_URL)) {
         print_r("Внимание!<br>Неккоректная ссылка!<br>Повторите ввод!");
         return;
     }
@@ -47,25 +49,47 @@ function link_to_reduce($url, $filename, $func1, $func2){
     $count_elems_in_array = count($file_array);
     var_dump($count_elems_in_array);
 
-    for($i = 0; $i < $count_elems_in_array; $i++){
-        if (stristr($file_array[$i], ' => ', true) == $url) {
-            print_r("Такая короткая ссылка уже существует! <br>". $short_url. "<br>");
-            $fp = fopen($filename, 'a');
-            $remade_short_url .= substr(strrev(md5($short_url)),0,3);
-            fwrite($fp, $remade_short_url);
-            fclose($fp);
-            print_r("Сгенерирована новая короткая ссылка: ! <br>". $short_url. $remade_short_url . "<br>");
+    $short_url_exists = 0;
+    $update_counter = 0;
+
+    for ($i = 0; $i <= $count_elems_in_array; $i++) {
+        if ($before_needle = stristr($file_array[$i], ' => ', true) == $url) {
+            print_r("Такая короткая ссылка уже существует! <br>" . $short_url . "<br>");
+            $short_url_exists++;
+//            $fp = fopen($filename, 'a');
+//            $new_short_url .= substr(strrev(md5($short_url)), 0, 3);
+//            fwrite($fp, $new_short_url);
+//            fclose($fp);
+//            print_r("Сгенерирована новая короткая ссылка: ! <br>". $short_url . $new_short_url . "<br>");
         }
     }
 
-    //Следущая часть кода не работает =(
-
-    for($i = 0; $i < $count_elems_in_array; $i++) {
-        if($count_elems_in_array === 0){
-            file_put_contents($filename, $url . " => " . $short_url . PHP_EOL, FILE_APPEND | LOCK_EX);
-            print_r("Создана новая короткая ссылка: ! <br>" . $short_url . "<br>");
+    if ($short_url_exists > 0) {
+        for ($i = 0; $i <= $count_elems_in_array; $i++) {
+            if ($after_needle = substr(stristr($file_array[$i], ' => '), 4)) {
+                $new_short_url = substr(strrev(md5($short_url)), 0, 3);
+                $after_needle .= $new_short_url;
+                $fp = fopen($filename, 'a');
+                fwrite($fp, $url . " => " . $after_needle.PHP_EOL);
+                fclose($fp);
+                print_r("Обновлена короткая ссылка:  <br>" . $after_needle . "<br>");
+                //            file_put_contents($file_array[$i], $new_short_url);
+                //$update_counter++;
+            }
         }
+    }
+
+    if (empty($file_array) && $short_url_exists == 0){
+        file_put_contents($filename, $url . " => " . $short_url . PHP_EOL, FILE_APPEND | LOCK_EX);
+        print_r("Создана новая короткая ссылка:<br>" . $short_url . "<br>");
     }
 }
 
+
 link_to_reduce($url, "links.txt", $get_short_link, $file_treatment);
+?>
+<div>
+    <a href="index.php">Попробовать снова!</a>
+</div>
+
+
